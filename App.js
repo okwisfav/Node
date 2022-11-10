ac77e0be87a550e7e31076ce44e5f42ef8a8b63d
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2022-11-10 14:31:39 +0100

    06

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 const names = require('./4-names');
 const sayHi = require('./05-utils');
+const data = require('./06-alternative-flavor');
+
+console.log(data);
 sayHi('favour')
 sayHi('John')
 sayHi('peter')

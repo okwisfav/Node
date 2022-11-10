f89f29683cfa1aab8b61c0a3db1152c4ad42bac3
Oghenekohworo Oghenefego <ofegofavour@gmail.com>
2022-11-10 10:50:04 +0100

    Modules

---
+++ 4-names.js
@@ -0,0 +1,6 @@
+const secret = 'Super Secert'
+const john = 'john'
+const  peter = 'peter'
+
+
+module.exports = {john, peter}
--- App.js
+++ App.js
@@ -1,10 +1,5 @@
-const john = 'john'
-const  peter = 'peter'
-
-const sayHi = (name) => {
-    console.log(`hello there ${name}`);
-}
-
+const names = require('./4-names');
+const sayHi = require('./05-utils');
 sayHi('favour')
 sayHi('John')
 sayHi('peter')
